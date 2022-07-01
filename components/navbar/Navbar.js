ec45ea60d68f14a0d096aff3b69a2446e04d6844
eds2002 <es23jr@gmail.com>
2022-07-01 19:24:19 -0400

    updates

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,21 +1,53 @@
 import Link from "next/link"
 import tw from "twin.macro"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 export default function Navbar(){
   return (
     <Container>
       <Main>
         <Nav>
+          <Left>
+            <IconsContainer>
+              <FontAwesomeIcon icon={faFacebookF} />
+              <FontAwesomeIcon icon={faInstagram} />
+            </IconsContainer>
+          </Left>
           <Middle>
             <Link href = "#">
               <Logo>Nüwa</Logo>
             </Link>
           </Middle>
+          <Right>
+            <Link href = "#">
+              <LinkName>Contacto</LinkName>
+            </Link>
+          </Right>
         </Nav>
       </Main>
     </Container>
   )
 }
+
+const Right = tw.div`
+flex-1
+flex
+items-center justify-end
+text-sm
+`
+
+const IconsContainer = tw.div`
+items-center justify-center gap-x-3
+hidden
+sm:flex
+`
+
+const Left = tw.div`
+flex-1
+flex items-center justify-start
+text-white
+`
 
 const Container = tw.div`
 absolute
@@ -43,7 +75,7 @@ flex items-center justify-center
 `
 
 const Logo = tw.h1`
-text-xl
+text-2xl
 font-medium
 text-[#EEE1D0]
 cursor-pointer
@@ -57,9 +89,14 @@ text-sm
 const LinkName = tw.a`
 text-[#F1E9DC]
 hover:text-[#E1B594]
+hover:border-[#E1B594]
 cursor-pointer
 transition
 ml-10
+px-4
+border-[1px]
+border-white
+rounded-xl
 `
 
 const Middle = tw.div`
